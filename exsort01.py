331b33950dd0d7870054104610902e672e664f3e
ixtr <74964010+ixtr@users.noreply.github.com>
2023-05-08 14:43:57 +0100

    Update exsort01.py

--- exsort01.py
+++ exsort01.py
@@ -3,5 +3,5 @@ with open("extendedwf02.txt", "r") as f:
     with open("exchange.txt", "w") as new_f:
         for line in lines:
             line_new = "<a href=\"https://github.com/wallerellei/?=" + line[5:]
-            line_new = line_new[:58] + "\"/>" + line_new[58:]
+            line_new = line_new[:58] + "\"/><br>" + line_new[58:]
             new_f.write(line_new + "\n")
