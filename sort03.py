c408090bde3f94b6e566e3496e13fd30cc61f0ef
ixtr <74964010+ixtr@users.noreply.github.com>
2023-05-24 10:20:57 +0100

    Update sort03.py

--- sort03.py
+++ sort03.py
@@ -12,7 +12,7 @@ for i in range(len(lines)):
     words = lines[i].split()
     for j in range(len(words)):
         if words[j] == '1701D0Ff039Fyy19vv11vd':
-            words[j] = '<a href="https://github.com/wallerellei/s?='
+            words[j] = '<a href="https://github.com/gustav4/s?='
         elif words[j] == 'tt67y2AH1719tt':
             words[j] = '">' 
         elif words[j] == '200667h235395719JL19a07f54ty': 
